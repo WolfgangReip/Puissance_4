69582b8a9d59cf9c3a62714397c0548822870ec1
rflowturing <wolf.reip@gmail.com>
2020-10-12 21:59:33 +0200

    finished stage 1

--- main.js
+++ main.js
@@ -4,15 +4,19 @@
 
 // game.player1Char = choseCharacter(1);
 // game.player2Char = choseCharacter(2);
-jeu.initialisation();
-jeu.showPuissance4();
 
 let joueurEnCours = 1;
 
 const tour = document.querySelector("#tour");
 const alert = document.querySelector(".alert");
+const messageJ1 = document.querySelector("#j1");
+const messageJ2 = document.querySelector("#j2");
+let pointJ1 = 0;
+let pointJ2 = 0;
 
 let endGame = false;
+
+initialisationTableau();
 // while (true) {
 //   if (play(1)) {
 //     console.log("Player 1 won");
@@ -66,8 +70,31 @@ function jouer(column) {
   // return game.endGameVerification(player);
 }
 
+function initialisationTableau() {
+  endGame = false;
+  joueurEnCours = 1;
+  alert.classList.add("d-none");
+  let contentJ1 = "<img src='J1.png' class='bg-warning rounded-circle'><br/>";
+  contentJ1 += pointJ1;
+  messageJ1.innerHTML = contentJ1;
+  let contentJ2 = "<img src='J2.png' class='bg-danger rounded-circle'><br/>";
+  contentJ2 += pointJ2;
+  messageJ2.innerHTML = contentJ2;
+
+  jeu.initialisation();
+  jeu.showPuissance4();
+}
+
 function handleendOfGame() {
   endGame = true;
-  alert.innerHTML = "End of game";
+  let contentAlert = "End of game, the winner is: " + joueurEnCours + "<br />";
+  contentAlert +=
+    '<button type="button" class="btn btn-secondary" onClick = initialisationTableau()>Restart</button>';
+  alert.innerHTML = contentAlert;
   alert.classList.remove("d-none");
+  if (joueurEnCours === 1) {
+    pointJ1++;
+  } else {
+    pointJ2++;
+  }
 }
